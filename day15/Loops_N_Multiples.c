7762e0d144c3c2199de0c3e238bede131c76354b
Roshan <roshan1601km@gmail.com>
2025-02-22 21:41:47 +0530

    day15

---
+++ day15/Loops_N_Multiples.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    
+    for(int i=1; i<=num1; ++i){
+        printf("%d ",i*num2);
+    }
+    return 0;
+}
